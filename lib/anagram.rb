7c64b3dac03d0d77b085895b48c7f5318486072a
Becky Zorn <bczorn@gmail.com>
2019-11-05 04:51:59 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,12 +3,20 @@
 class Anagram
 
 attr_accessor 
+@@all
 
 def initialize(word)
   @word = word
 end
 
 def match(word_array)
-  word_array.each do 
+  word_array.each do |i|
+    ii = i.split.sort
+    ww = @word.split.sort
+    if ii == ww
+      @@all << i 
+      return i
+    end
+  end
   
 end
